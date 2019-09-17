7d423c6f2cc57c109851e2aa65f6c902790074e9
Andrew Kim <wk@ualberta.ca>
2019-09-17 21:55:28 +0000

    added counts for product and categories

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,16 @@
 class Admin::DashboardController < ApplicationController
   def show
   end
+
+  private
+
+  def product_list
+    @product_list = Product.all
+  end
+  helper_method :product_list
+
+  def category_list
+    @category_list = Category.all
+  end
+  helper_method :category_list
 end
