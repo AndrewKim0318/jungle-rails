b192e1aec9d0d8087da25827a9eeca94919580e5
Andrew Kim <wk@ualberta.ca>
2019-09-23 15:27:14 +0000

    checked error messages for product creation

--- spec/models/product_spec_spec.rb
+++ spec/models/product_spec_spec.rb
@@ -4,25 +4,29 @@ RSpec.describe Product, type: :model do
   describe 'Validations' do
     it "create a new product" do
       newCategory = Category.create(name: "test")
-      puts newCategory.id
-      newProduct = Product.create(name: "shoes", price: 600, quantity: 5, category: Category.find(9))
+      newProduct = Product.create(name: "shoes", price: 600, quantity: 5, category: Category.find(newCategory.id))
 
       expect(newProduct).to be_valid
 
       newProduct.name = nil
       expect(newProduct).to_not be_valid
+      expect(newProduct.errors.full_messages).to eq ["Name can't be blank"]
 
       newProduct.price_cents = nil
       newProduct.name = "shoes"
       expect(newProduct).to_not be_valid
-
+      expect(newProduct.errors.full_messages).to eq ["Price cents is not a number", "Price is not a number", "Price can't be blank"]
+      
       newProduct.price_cents = 600
       newProduct.quantity = nil
       expect(newProduct).to_not be_valid
-
+      expect(newProduct.errors.full_messages).to eq ["Quantity can't be blank"]
+      
       newProduct.quantity = 5
       newProduct.category = nil
       expect(newProduct).to_not be_valid
+      expect(newProduct.errors.full_messages).to eq ["Category can't be blank"]
+      puts newProduct.errors.full_messages
     end
   end
 end
